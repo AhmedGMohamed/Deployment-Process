6effeb55f11a7c6d16abf6b6093e2c3b6dc22da5
AhmedGMohamed <gamal_aharam3@yahoo.com>
2022-05-20 18:42:43 +0200

    added backend api link

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost:
+    'http://udacity-project-backend-prod.eba-nyskwvky.eu-central-1.elasticbeanstalk.com/',
 };
 
 /*
