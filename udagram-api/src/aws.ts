2685a2ddd8fcc83eb77eeda2e6469f014184d568
AhmedGMohamed <gamal_aharam3@yahoo.com>
2022-05-24 16:38:07 +0200

    rollback

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -12,22 +12,21 @@ export const s3 = new AWS.S3({
 // Generates an AWS signed URL for retrieving objects
 export function getGetSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
-  const funcParams = {
+
+  return s3.getSignedUrl("getObject", {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
-  };
-
-  return s3.getSignedUrl("getObject", funcParams);
+  });
 }
 
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
-  const funcParams = {
+  
+  return s3.getSignedUrl("putObject", {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
-  };
-  return s3.getSignedUrl("putObject", funcParams);
+  });
 }
